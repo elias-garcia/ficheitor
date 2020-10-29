8aa66f7408adbd27f48178addfe6bc00cd683a2c
Elias <eliasgarciadev@gmail.com>
2020-10-29 14:47:39 +0100

    Fix utils getSlotHour fn

--- src/utils.js
+++ src/utils.js
@@ -8,21 +8,29 @@ function getTodayDate() {
   return dayjs().format("YYYY-MM-DD");
 }
 
+function padLeft(value) {
+  if (value >= 10) {
+    return value.toString();
+  }
+
+  return `0${value}`;
+}
+
 function getSlotHour(slot) {
   const todayDate = getTodayDate();
 
   switch (slot) {
     case 1: {
-      return `${todayDate}T${process.env.START_HOUR}:00:00+01:00`;
+      return `${todayDate}T${padLeft(process.env.START_HOUR)}:00:00+01:00`;
     }
     case 2: {
-      return `${todayDate}T${process.env.BREAK_START_HOUR}:00:00+01:00`;
+      return `${todayDate}T${padLeft(process.env.BREAK_START_HOUR)}:00:00+01:00`;
     }
     case 3: {
-      return `${todayDate}T${process.env.BREAK_END_HOUR}:00:00+01:00`;
+      return `${todayDate}T${padLeft(process.env.BREAK_END_HOUR)}:00:00+01:00`;
     }
     case 4: {
-      return `${todayDate}T${process.env.END_HOUR}:00:00+01:00`;
+      return `${todayDate}T${padLeft(process.env.END_HOUR)}:00:00+01:00`;
     }
   }
 }
